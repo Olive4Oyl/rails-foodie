85b4bb8f32ff6cb2ae470e6a04a9391273e63928
Kelene Chung <simoy93@gmail.com>
2016-10-27 10:50:05 -0400

    got the OpenTable API working in the controllers and corrected the user sign in and out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  require "models/open_table"
+  include OpenTable
+
+  def after_sign_in_path_for(resource)
+  	request.env['omniauth.origin'] || root_path
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :reservations
   resources :itineraries
   resources :restaurants
   resources :destinations
   resources :user_destinations
 
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
