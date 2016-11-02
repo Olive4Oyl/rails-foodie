5683de3d6ce766f03dd715ab24e876f8edf1748c
Kelene Chung <simoy93@gmail.com>
2016-11-02 10:00:48 -0400

    updated the validations

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -5,6 +5,7 @@ class Itinerary < ApplicationRecord
 	has_many :reservations
 	has_many :restaurants, through: :reservations
 	accepts_nested_attributes_for :reservations
+	validates :name, presence: true
 
 	scope :recent, -> { order("itineraries.updated_at DESC") }
 
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,5 @@
 class Reservation < ApplicationRecord
 	belongs_to :itinerary, optional: true
 	belongs_to :restaurant, optional: true
+	validates :reserved_time, numericality: { only_integer: true }
 end
